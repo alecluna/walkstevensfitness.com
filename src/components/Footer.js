48c06a0667ca2fe24364c4fe29a0f9b6ece31f1a
Alec Luna <alecluna@csus.edu>
2018-11-19 22:19:45 -0800

    styles and filler text for client Testimonies page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,53 @@
 import React from 'react'
 import Typography from '../../node_modules/@material-ui/core/Typography'
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
 
 const styles = {
   footerDemensions: {
-    height: '150px',
-    backgroundColor: '#282828'
+    height: '120px',
+    maxWidth: '100%',
+    backgroundColor: '#282828',
+    color: 'white',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  icontStyles: {
+    height: '2em',
+    width: '2em',
+    borderRadius: '50px'
   }
 }
 export default function Footer() {
   return (
     <div style={{ marginTop: '10%' }}>
       <section style={styles.footerDemensions}>
-        <Typography> Test</Typography>
+        <Typography> Contact Me:</Typography>
+        <div
+          style={{
+            width: '200px',
+            display: 'flex',
+            justifyContent: 'space-around',
+            alignItems: 'center'
+          }}
+        >
+          <div />
+          <div>
+            <a href="https://www.instagram.com/walkstevens/ ">
+              <FaInstagram color="white" size="2em" />
+            </a>
+          </div>
+          <div>
+            <a href="https://www.twitter.com">
+              <FaTwitter color="white" size="2em" />
+            </a>
+          </div>
+          <div>
+            <a href="https://www.facebook.com">
+              <FaFacebook color="white" size="2em" />
+            </a>
+          </div>
+        </div>
       </section>
       <div style={{ textAlign: 'center', backgroundColor: '#202020' }}>
         <Typography style={{ color: 'white' }}>
