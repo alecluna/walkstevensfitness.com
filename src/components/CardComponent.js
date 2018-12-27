081f6d99e7bcdf724a9d3f7c0fa89f13c2a9b4ca
Alec Luna <alecluna@csus.edu>
2018-12-27 13:11:30 -0800

    removed all pages to troublshoot netlify

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import Card from '@material-ui/core/Card'
 import Typography from '@material-ui/core/Typography'
 import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
 import Link from 'gatsby-link'
 
 const styles = {
