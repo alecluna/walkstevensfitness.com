3058805d8474f345c359d0de3dbc23869f67d6be
Alec Luna <alecluna@csus.edu>
2018-12-31 14:07:37 -0800

    more media queries added

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,7 +5,7 @@ import Border from '../components/border'
 import styled from '../../node_modules/styled-components'
 
 const device = {
-  mobileS: `(min-width: 320px)`,
+  mobileS: `(max-device-width < 425px)`,
   mobileM: `(min-device-width: 375px)`,
   mobileL: `(min-width: 425px)`,
   tablet: `(min-width: 768px)`,
@@ -23,8 +23,8 @@ const styles = {
     alignItems: 'center'
   },
   paperStyle: {
-    height: '55%',
-    width: '80%',
+    height: '50%',
+    width: '70%',
     marginTop: '15%'
   }
 }
@@ -33,10 +33,27 @@ const PaperWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: 5%;
+`
 
+const CustomTypography = styled.div`
+  color: black;
+  font-weight: 240;
+  font-size: 1.9em;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5%;
+  text-align: center;
+
+  /* iPhone 6 7 & 8 Portrait */
+  @media only screen and (min-device-width: 414px) and (max-device-width: 736px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: portrait) {
+    margin-top: 15%;
+  }
+  /*small devices portriat */
   @media ${device.mobileS} {
-    margin-top: 10%;
+    margin-top: 15%;
   }
 
   /* iPhone 6 7 & 8 Landscape */
@@ -49,24 +66,17 @@ const PaperWrapper = styled.div`
     margin-top: 2%;
   }
 `
+
 export default class Hero extends Component {
   render() {
     return (
       <div style={styles.background}>
         <Paper style={styles.paperStyle} elevation={20}>
           <PaperWrapper>
-            <Typography
-              variant="display4"
-              style={{
-                textAlign: 'center',
-                color: 'black',
-                fontWeight: '250',
-                fontSize: '2em'
-              }}
-            >
+            <CustomTypography variant="display4">
               {this.props.text}
               <Border />
-            </Typography>
+            </CustomTypography>
           </PaperWrapper>
         </Paper>
       </div>
