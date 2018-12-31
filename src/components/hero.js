d34bd661957e755d8eb2b2050e6eef0e3d774100
Alec Luna <alecluna@csus.edu>
2018-12-31 13:37:10 -0800

    added styled components to Hero section for mobile styling

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 import Typography from '../../node_modules/@material-ui/core/Typography'
 import Paper from '../../node_modules/@material-ui/core/Paper'
 import Border from '../components/border'
+import styled from '../../node_modules/styled-components'
+
+const device = {
+  mobileS: `(min-width: 320px)`,
+  mobileM: `(min-device-width: 375px)`,
+  mobileL: `(min-width: 425px)`,
+  tablet: `(min-width: 768px)`,
+  laptop: `(min-width: 1024px)`,
+  laptopL: `(min-width: 1440px)`,
+  desktop: `(min-width: 2560px)`
+}
 
 const styles = {
   background: {
@@ -12,26 +23,51 @@ const styles = {
     alignItems: 'center'
   },
   paperStyle: {
-    height: '60%',
+    height: '55%',
     width: '80%',
     marginTop: '15%'
   }
 }
 
+const PaperWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5%;
+
+  @media ${device.mobileS} {
+    margin-top: 10%;
+  }
+
+  /* iPhone 6 7 & 8 Landscape */
+  @media only screen and ${device.mobileM} and (max-device-width: 667px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: landscape) {
+    margin-top: 2%;
+  }
+
+  /* iPhone X Landscape */
+  @media only screen and ${device.mobileM} and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3) and (orientation: landscape) {
+    margin-top: 2%;
+  }
+`
 export default class Hero extends Component {
   render() {
     return (
       <div style={styles.background}>
         <Paper style={styles.paperStyle} elevation={20}>
-          <div style={{ textAlign: 'center', marginTop: '50px' }}>
+          <PaperWrapper>
             <Typography
               variant="display4"
-              style={{ color: 'black', fontWeight: '200', fontSize: '2em' }}
+              style={{
+                textAlign: 'center',
+                color: 'black',
+                fontWeight: '250',
+                fontSize: '2em'
+              }}
             >
               {this.props.text}
+              <Border />
             </Typography>
-            <Border />
-          </div>
+          </PaperWrapper>
         </Paper>
       </div>
     )
