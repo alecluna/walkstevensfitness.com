c3fc4d7e29f7a39bc3e24e67b2143c5da18ad19c
Alec Luna <alecluna@csus.edu>
2019-01-04 14:05:53 -0800

    changed name of image file

--- src/pages/ClientHowTos.js
+++ src/pages/ClientHowTos.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import CardComponent from '../components/cardcomponent'
 
-import Biceps from '../../assets/Biceps.jpeg'
+import Biceps from '../../assets/biceps.jpeg'
 // import Core from '../../assets/Core.jpeg'
 // import Shoulders from '../../assets/Shoulders.jpg'
 // import Legs from '../../assets/Legs.jpg'
