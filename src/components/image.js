9694e604b11ae7bf819946d3b3abdd88c3b3c21d
Alec Luna <alecluna@csus.edu>
2019-01-21 19:45:18 -0800

    removed border radius for all images, removed typo css

--- src/components/image.js
+++ src/components/image.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Image = ({ image }) => (
   <div>
     <img
-      style={{ hight: '500px', width: '500px' }}
+      style={{ width: '500px' }}
       className="grow"
       src={image}
       alt="Fitness Card Component"
